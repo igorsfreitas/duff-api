634ae2247d22799982f40fafbc33cab2482c794a
Igor Freitas <igorfreitas1992@gmail.com>
2023-04-04 18:11:58 -0300

    :wrench: chore: config app port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dot from 'dotenv';
 
 async function bootstrap() {
+  dot.config();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3333);
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
